1dbb4f42faeacc3702e6c4e7535141c40df6c19b
GeomTech <geomtech@users.noreply.github.com>
2017-09-25 16:19:33 +0200

    ADD GET SYSID, CREATE, MODIFY, UPDATE, DELETE

--- SNClient/SNClient.php
+++ SNClient/SNClient.php
@@ -1,6 +1,7 @@
 <?php
 /* ServiceNow Client Class
  * author: Alexy DA CRUZ (GeomTech): dacruzalexy@gmail.com
+ * Available on Github: https://github.com/geomtech/Service-Now-PHPClient
 */
 
 class ServiceNowClient
@@ -19,55 +20,73 @@ class ServiceNowClient
         $this->password = $password;
     }
 
-    /* Test authentification on Service-Now by getting incident table with a limit of 10 incidents.
+    /* Test authentification on Service-Now by getting incident table with a limit of 1 incident.
      * If return false, authentification failed.
     */
     public function Authenticated(){
-        if(!$this->GETFromTable("incident", "", true)){
-          return false;
-        } else {
-          return true;
-        }
+      $RESTReq = 'https://'.$this->instance.'.service-now.com/api/now/table/incident?sysparm_limit=1';
+
+      $ch = curl_init($RESTReq);
+      curl_setopt($ch, CURLOPT_USERPWD, $this->username . ":" . $this->password);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Accept: application/json',
+        'Content-Type: application/json',
+      ));
+      $get = curl_exec($ch);
+      $ResponseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+      if($ResponseCode == 200){
+        return true;
+      } else {
+        return false;
+      }
     }
 
     /* Get the table from Service-Now
+     * $table: Name of the table
      * $parameters: parameters from Service-Now like "?sysparm_limit=10", "?sysparm_query=number=INC000001" , etc.. (nothing by default)
-     * $auth: Useful to do a query for testing authentification (false by default)
      *
      * Return false if authentification failed.
      * If authentification works, it return a json object.
     */
-    public function GETFromTable($table, $parameters = "", $auth = false) {
-      if(empty($table)){
-        throw new ErrorException('$table is empty in the function \'GETFromTable().\'');
+    public function RetrieveAllRecords($table, $parameters = "") {
+      if(!$parameters == ""){
+        $RESTReq = 'https://'.$this->instance.'.service-now.com/api/now/table/'.$table.'?'.$parameters;
+      } else {
+        $RESTReq = 'https://'.$this->instance.'.service-now.com/api/now/table/'.$table;
       }
 
-      if($auth){
-        $RESTReq = 'https://'.$this->username.':'.$this->password.'@'.$this->instance.'.service-now.com/api/now/table/'.$table.'?sysparm_limit=10';
-      } else {
-        if(!$parameters == ""){
-          $RESTReq = 'https://'.$this->username.':'.$this->password.'@'.$this->instance.'.service-now.com/api/now/table/'.$table.'?'.$parameters;
-        } else {
-          $RESTReq = 'https://'.$this->username.':'.$this->password.'@'.$this->instance.'.service-now.com/api/now/table/'.$table;
-        }
-      }
+      $ch = curl_init($RESTReq);
+      curl_setopt($ch, CURLOPT_USERPWD, $this->username . ":" . $this->password);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Accept: application/json',
+        'Content-Type: application/json',
+      ));
 
-      $get = @file_get_contents($RESTReq);
-      if($get == '{"error":{"detail":"Required to provide Auth information","message":"User Not Authenticated"},"status":"failure"}'){
-        return false;
-      } else {
+      $get = curl_exec($ch);
+
+      $ResponseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+      if($ResponseCode == 200){
         $json = json_decode($get);
         return $json;
+      } else {
+        return false;
       }
     }
 
-    /* Create an new record on Service-Now
+    /* Create a new record on Service-Now
+     * $table: Name of the table
      * $data: Array with all fields you want to insert
      *
      * Return false if the request have an error
      * Return true if the request completed successfully
     */
-    public function POSTInTable($table = "", $data)
+    public function CreateRecord($table, $data)
     {
       $data_string = json_encode($data);
       $url = 'https://'.$this->instance.'.service-now.com/api/now/table/'.$table;
@@ -87,6 +106,138 @@ class ServiceNowClient
       $ResponseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
       if($ResponseCode == 201){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    /* Get a record from a Table
+     * $table: Name of the table
+     * $ID: ID of the record (incident, task)
+     *
+     * Return false if authentification failed.
+     * If authentification works, it return a json object.
+    */
+    public function RetrieveRecord($table, $ID) {
+      $RESTReq = 'https://'.$this->instance.'.service-now.com/api/now/table/'.$table.'/'.$ID;
+
+      if(empty($ID)){
+        return false;
+      }
+
+      $ch = curl_init($RESTReq);
+      curl_setopt($ch, CURLOPT_USERPWD, $this->username . ":" . $this->password);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Accept: application/json',
+        'Content-Type: application/json',
+      ));
+
+      $get = curl_exec($ch);
+
+      $ResponseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+      if($ResponseCode == 200){
+        $json = json_decode($get);
+        return $json;
+      } else {
+        return false;
+      }
+    }
+
+    /* Modify record on Service-Now
+     * $table: Name of the table
+     * $sys_id: Sys_ID of the record to modify
+     * $data: Array with all fields you want to insert
+     *
+     * Return false if the request have an error
+     * Return true if the request completed successfully
+    */
+    public function ModifyRecord($table, $sys_id, $data)
+    {
+      $data_string = json_encode($data);
+      $url = 'https://'.$this->instance.'.service-now.com/api/now/table/'.$table.'/'.$sys_id;
+
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_USERPWD, $this->username . ":" . $this->password);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Accept: application/json',
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data_string))
+      );
+
+      $result = curl_exec($ch);
+      $ResponseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+      if($ResponseCode == 201){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    /* Update record on Service-Now
+     * $sys_id: Sys_ID of the record to update
+     * $table: Name of the table
+     * $data: Array with all fields you want to insert
+     *
+     * Return false if the request have an error
+     * Return true if the request completed successfully
+    */
+    public function UpdateRecord($table, $sys_id, $data)
+    {
+      $data_string = json_encode($data);
+      $url = 'https://'.$this->instance.'.service-now.com/api/now/table/'.$table.'/'.$sys_id;
+
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_USERPWD, $this->username . ":" . $this->password);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PATCH");
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Accept: application/json',
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data_string))
+      );
+
+      $result = curl_exec($ch);
+      $ResponseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+      if($ResponseCode == 200){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    /* Delete record on Service-Now
+     * $sys_id: Sys_ID of the record to delete
+     * $table: Name of the table
+     *
+     * Return false if the request have an error
+     * Return true if the request completed successfully
+    */
+    public function DeleteRecord($table, $sys_id)
+    {
+      $url = 'https://'.$this->instance.'.service-now.com/api/now/table/'.$table.'/'.$sys_id;
+
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_USERPWD, $this->username . ":" . $this->password);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Accept: application/json'
+      ));
+
+      $result = curl_exec($ch);
+      $ResponseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+      if($ResponseCode == 204){
         return true;
       } else {
         return false;
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 /* Index example
  * author: Alexy DA CRUZ (GeomTech): dacruzalexy@gmail.com
+ * Available on Github: https://github.com/geomtech/Service-Now-PHPClient
 */
 
 require_once("./SNClient/SNClient.php");
@@ -9,19 +10,14 @@ $SNClient = new ServiceNowClient("dev39869", "david.loo", "devatgl");
 if($SNClient->Authenticated()){
 
   $data_array = array(
-    'short_description' => '$short_description',
-    'description' => '$description',
-    'urgency' => "1",
-    'impact' => "1",
-    'state' => "1",
-    'number' => "INC9000001",
+    'short_description' => 'modified',
   );
 
-  $SNClient->POSTInTable("incident", $data_array);
+  $SNClient->ModifyRecord("incident", "9c573169c611228700193229fff72400", $data_array);
 
-  foreach($SNClient->GETFromTable("incident", "sysparm_query=number=INC9000001")->result as $incidents)
+  foreach($SNClient->RetrieveRecord("incident", "9c573169c611228700193229fff72400") as $incidents)
   {
-    echo $incidents->number.'<br>';
+    echo $incidents->short_description.'<br>';
   }
   echo '<br>';
 }
